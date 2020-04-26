2d43b8cd2417bf536a992c180ac7e7fed71a3c9c
lejdiprifti <priftilejdi@gmail.com>
2020-04-26 02:16:48 +0200

    customize question admin interface

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Question
 
-admin.site.register(Question)
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
